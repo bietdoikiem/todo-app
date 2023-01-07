c9c37e0016ffa78caea8777ef0ee777c43b1b97a
bietdoikiem <minhlucky2408@gmail.com>
2023-01-07 17:38:10 +0700

    refactor: update shell script

--- set-params.sh
+++ set-params.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-export PG_USER="$(aws ssm get-parameters --names /todo-app/production/pg_user --query 'Parameters[0].Value' --output text)"
-export PG_PASSWORD="$(aws ssm get-parameters --names /todo-app/production/pg_password --query 'Parameters[0].Value' --output text)"
-export PG_DATABASE="$(aws ssm get-parameters --names /todo-app/production/pg_database --query 'Parameters[0].Value' --output text)"
-export PG_HOST="$(aws ssm get-parameters --names /todo-app/production/pg_host --query 'Parameters[0].Value' --output text)"
-export PG_PORT="$(aws ssm get-parameters --names /todo-app/production/pg_port --query 'Parameters[0].Value' --output text)"
-export JWT_SECRET="$(aws ssm get-parameters --names /todo-app/production/jwt_secret --query 'Parameters[0].Value' --output text)"
+export PG_USER="$(aws ssm get-parameters --region us-east-1 --names /todo-app/production/pg_user --query 'Parameters[0].Value' --output text)"
+export PG_PASSWORD="$(aws ssm get-parameters --region us-east-1 --names /todo-app/production/pg_password --query 'Parameters[0].Value' --output text)"
+export PG_DATABASE="$(aws ssm get-parameters --region us-east-1 --names /todo-app/production/pg_database --query 'Parameters[0].Value' --output text)"
+export PG_HOST="$(aws ssm get-parameters --region us-east-1 --names /todo-app/production/pg_host --query 'Parameters[0].Value' --output text)"
+export PG_PORT="$(aws ssm get-parameters --region us-east-1 --names /todo-app/production/pg_port --query 'Parameters[0].Value' --output text)"
+export JWT_SECRET="$(aws ssm get-parameters --region us-east-1 --names /todo-app/production/jwt_secret --query 'Parameters[0].Value' --output text)"
